16b19b6051f7c12453ee623062b7650922de0e51
Lucas <lucascanellasdev@gmail.com>
2022-08-07 00:21:04 -0300

    feat: att app and main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import styles from "./App.module.css";
+import { BodySection } from "./components/BodySection/BodySection";
 import { HeaderSection } from "./components/HeaderSection/HeaderSection";
 
 export default function App() {
   return (
-    <main className={styles.main}>
-      <HeaderSection />
-    </main>
+    <>
+      <main className={styles.main}>
+        <HeaderSection />
+        <BodySection />
+      </main>
+    </>
   );
 }
